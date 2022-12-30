3b8d9fd5f2a05eb207e59fc8736539c589d47525
Dinesh <codelearntry@outlook.com>
2022-12-30 11:33:29 +0530

    Handle update

--- app/(home)/[event]/RegistrationForm.tsx
+++ app/(home)/[event]/RegistrationForm.tsx
@@ -411,7 +411,7 @@ const RegistrationForm = ({
               disabled={loading}
               type='submit'
               className='text-white w-48 border border-green-600 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm  px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 disabled:bg-gray-300 disabled:text-gray-700'>
-              Register
+              {attendee && !attendee.confirmed ? 'Update' : 'Register'}
             </button>
             <button
               type='button'
