b8dc04a549f19f906a5230d0e587d2e9a7e21f42
Dinesh <codelearntry@outlook.com>
2023-01-05 11:55:36 +0530

    deployment issue

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,12 @@
 import { redirect } from 'next/navigation'
 
 export default async function Events() {
-  redirect('/thana')
   // const supabase = createClient()
   // const session = await supabase.auth.getSession()
-
   // const { data } = await supabase.from('events.event').select('*')
-
-  // return (
-  //   <div className='flex flex-col items-center justify-center gap-6 '>
-  //     {data && data.map((x, index) => <EventCard key={index} event={x} />)}
-  //   </div>
-  // )
+  return (
+    <div className='flex flex-col items-center justify-center gap-6 '>
+      {redirect('/thana')}
+    </div>
+  )
 }
